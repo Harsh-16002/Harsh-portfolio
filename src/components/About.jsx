8caaf498057b54398fe623c51b811c55a4a6080c
Harsh-16002 <harshvermapc5@gmail.com>
2025-11-29 15:39:49 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-       I'm a skilled software developer with experience in JavaScript and Python, 
-       specializing in frameworks like React and Node.js. 
-       I'm a quick learner and collaborate closely with clients to build efficient, 
-       scalable, and user-friendly solutions that solve real-world problems.
-        Let’s bring your ideas to life!
+        I’m a dedicated software developer focused primarily on Laravel and PHP, with growing experience in JavaScript and Python. I specialize in building clean, reliable, and efficient backend systems, and I’m currently strengthening my expertise through hands-on work and internships in Laravel development. I’m a fast learner, always improving, and I collaborate closely with clients to create scalable, user-friendly solutions that solve real-world problems.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
